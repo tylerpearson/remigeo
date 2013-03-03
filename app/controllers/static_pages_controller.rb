8c8506057d55f4000d19b2487e05d3760fda176f
Tyler Pearson <ty.pearson@gmail.com>
2013-03-03 16:17:34 -0500

    some homepage formatting

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,8 +8,7 @@ class StaticPagesController < ApplicationController
 
   def home
     unless current_user.nil?
-      result = request.location
-      @locations = current_user.locations.near(result.coordinates, 1, :order => :distance).limit(20)
+      @messages = current_user.messages.limit(5)
     end
     respond_to do |format|
       format.html # home.html.erb
