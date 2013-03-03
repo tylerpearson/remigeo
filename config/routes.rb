1f718ef116394c24b3408270527bfbe4d498470b
Tyler Pearson <ty.pearson@gmail.com>
2013-03-02 22:42:27 -0500

    user authentication setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ MessageLocation::Application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get "login", :to => "devise/sessions#new"
-    get "signup", :to => "devise/registrations#new"
+    get "login",   :to => "devise/sessions#new"
+    get "signout", :to => "devise/sessions#destroy"
+    get "signup",  :to => "devise/registrations#new"
   end
 
   root :to => "static_pages#home"
