4a3d9cf70cb8369f5bb1902c783df24348780e70
Tyler Pearson <ty.pearson@gmail.com>
2013-03-05 20:12:33 -0500

    add in font awesome social and header links

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  has_many :messages, dependent: :destroy
-  has_many :locations, through: :messages
+  has_many :messages, :dependent => :destroy
+  has_many :locations, :through => :messages
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
 
 
 end
