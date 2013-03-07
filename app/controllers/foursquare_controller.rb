cf2627c9d4f1fb7662c87b20bf23414b1629d099
Tyler Pearson <ty.pearson@gmail.com>
2013-03-06 23:54:12 -0500

    checkin controller tweaks

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,23 +1,26 @@
 class FoursquareController < ApplicationController
 
+  before_filter :protect_from_forgery, :except => [:push]
+
   def push
-    if ActiveSupport::JSON.decode(params[:secret]) == ENV["FOURSQUARE_PUSH_SECRET"]
+    #if ActiveSupport::JSON.decode(params[:secret]) == ENV["FOURSQUARE_PUSH_SECRET"]
 
-      checkin_hash = ActiveSupport::JSON.decode(params[:checkin])
+    checkin_hash = ActiveSupport::JSON.decode(params[:checkin])
 
-      user   = User.find_by_uid(checkin_hash["user"]["id"])
-      type = checkin_hash["type"]
+    #user   = User.find_by_uid(checkin_hash["user"]["id"])
+    #type   = checkin_hash["type"]
 
-      if user
-        checkin = Checkin.find_by_checkin_id(checkin_hash["id"])
-        checkin = Checkin.new( :user => user, :action => type, :checkin_id =>  checkin_hash["id"]) unless checkin
-        #checkin.set_checkin_data checkin_hash
-        checkin.save
-        #checkin.process_checkin
-      end
+    checkin = Checkin.create(user: checkin_hash["user"]["id"], action: checkin_hash["type"], checkin_id: checkin_hash["id"]) #unless checkin
 
-      render :text => "success"
-    end
+    #if user
+      #checkin = Checkin.find_by_checkin_id(checkin_hash["id"])
+      #checkin.set_checkin_data checkin_hash
+      #checkin.save
+      #checkin.process_checkin
+    #end
+
+    render :text => "success"
+    #end
   end
 
 
