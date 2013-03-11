f9abe3f5ebdee1b39360b2f3303dd92f3b924c36
Tyler Pearson <ty.pearson@gmail.com>
2013-03-10 23:57:04 -0400

    more checkin page setup

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -2,10 +2,14 @@ class CheckinsController < ApplicationController
 
   def show
 
+    # alls the info, should probably be cleaned up at some point
     @checkin = Checkin.find_by_unique_checkin_slug(params[:checkinid])
     @user = User.find(@checkin.user_id)
     @location = Location.find_by_foursquare_location_id(@checkin.foursquare_location_id)
     @messages = @user.messages.where("location_id <= ?", @location.id)
+
+    # for adding a new one
+    @message = Message.new
 
   end
 
