ac413d0a87d92303aea35cddc1353db1e9520a66
Tyler Pearson <ty.pearson@gmail.com>
2013-03-12 00:07:46 -0400

    fix message search for checkin page

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -8,7 +8,7 @@ class CheckinsController < ApplicationController
     @checkin  = Checkin.find_by_unique_checkin_slug(params[:checkinid])
     @user     = User.find(@checkin.user_id)
     @location = Location.find_by_foursquare_location_id(@checkin.foursquare_location_id)
-    @messages = @user.messages.where("location_id <= ?", @location.id)
+    @messages = @user.messages.where("location_id = ?", @location.id)
 
     # for adding a new one
     @message = Message.new
