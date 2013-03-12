4e3f803ac7e238d5893873cf1fe48b2f29f19113
Tyler Pearson <ty.pearson@gmail.com>
2013-03-11 21:02:51 -0400

    model cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,8 @@ class User < ActiveRecord::Base
   has_many :checkins
 
   def apply_omniauth(auth)
-    # In previous omniauth, 'user_info' was used in place of 'raw_info'
     self.email = auth['extra']['raw_info']["contact"]["email"]
-    # Again, saving token is optional. If you haven't created the column in authentications table, this will fail
     authentications.build(:provider => auth['provider'], :uid => auth['uid'], :token => auth['credentials']['token'])
   end
-
 
 end
